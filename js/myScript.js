badc0c3a9d2e141681ca30359f14e6175f9aba78
kamnyl <kamillanylund@gmail.com>
2025-03-19 13:02:07 +0100

    Added JavaScript

---
+++ js/myScript.js
@@ -0,0 +1,20 @@
+// 1
+console.log('Hello World!!');
+
+
+// 2
+let name = 'Kamilla';
+let age = '29';
+let color = 'green';
+
+console.log(`My name is ${name}. I am ${age} years old and my favorite color is ${color}.`);
+
+
+// 3
+let x = 20;
+let y = 5;
+
+console.log('Addition:', x + y)
+console.log('Subtraction:', x - y);
+console.log('Multiplication:', x * y)
+console.log('Division:', x / y)
